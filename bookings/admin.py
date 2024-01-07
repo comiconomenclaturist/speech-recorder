f510b510cfe8002c4efc7cdfcaecfc8b5469cd35
comiconomenclaturist <james@4thharmonic.com>
2024-01-07 11:27:13 +0000

    Ignoring dumpdata files in json format. Created an admin class for the Bookings model

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from bookings.models import Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    model = Booking
+
+    def start(self, obj):
+        if obj:
+            return obj.session.lower
+
+    def end(self, obj):
+        if obj:
+            return obj.session.upper
+
+    list_display = ("start", "end", "speaker")
