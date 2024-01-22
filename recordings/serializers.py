d913050e987edb3c84121657701ff210151949cf
comiconomenclaturist <james@4thharmonic.com>
2024-01-22 18:48:04 +0000

    Removed the speaker from the Script model so removing it from the serializer too

--- recordings/serializers.py
+++ recordings/serializers.py
@@ -129,4 +129,4 @@ class ScriptSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Script
-        exclude = ("speaker", "id", "project")
+        exclude = ("id", "project")
